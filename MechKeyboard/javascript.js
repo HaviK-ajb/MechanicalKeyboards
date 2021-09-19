75e540dc4fab7f16c4d4cd1d0e7ffb811679546d
Adam Bourne <47788820+HaviK-ajb@users.noreply.github.com>
2021-09-19 18:58:14 +0100

    Add files via upload

---
+++ MechKeyboard/javascript.js
@@ -0,0 +1,11 @@
+/* Set the width of the sidebar when shown */
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "40vh";
+  }
+  
+  /* Set the width of the sidebar to 0 (hide it) */
+  function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+  }
+
+
